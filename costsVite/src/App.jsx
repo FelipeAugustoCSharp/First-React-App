e2990ad96209c9164c504221cb2e4dca2d677a0f
Kammai <felipeaugustobonfimdealmeida@gmail.com>
2024-04-26 22:08:55 -0300

    feat(pages): criacao e adicao de novas pagina, além da criação de roteamento

--- costsVite/src/App.jsx
+++ costsVite/src/App.jsx
@@ -1,14 +1,38 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import './App.css'
-import Home from './components/Home';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Contato from './components/pages/Contato';
+import Empresa from './components/pages/Empresa';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
 
 function App() {
-
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/contato">Contato</Link>
+          <Link to="/empresa">Empresa</Link>
+          <Link to="/newproject">New Project</Link>
+        </ul>
+      </div>
+
+
+    <Container customClass="min-height">
+      <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/contato" element={<Contato />}/>
+          <Route path="/empresa" element={<Empresa />}/>
+          <Route path="/newproject" element={<NewProject />}/>
+      </Routes>
+    </Container>
+
+      <p>Footer</p>
+    </Router>
+
   )
 }
 
---
+++ costsVite/src/components/layout/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <div>
+        <Link to="/">Home</Link>
+        <Link to="/contato">Contato</Link>
+        <Link to="/empresa">Empresa</Link>
+        <Link to="/newproject">New Project</Link>
+    </div>
+
+    
+  )
+}
+export default Navbar
