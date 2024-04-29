8d9173a66b30343d791a400ffbdf7eb7f400acc0
Kammai <felipeaugustobonfimdealmeida@gmail.com>
2024-04-28 23:50:02 -0300

    feat(back: API, page: NewProject): Criação da funcionalidade para adicionar itens na lista de projetos

--- costsVite/src/components/ci/Select.jsx
+++ costsVite/src/components/ci/Select.jsx
@@ -1,16 +1,23 @@
-import styles from '../styles/Select.module.css'
+import styles from "../styles/Select.module.css";
 
 function Select({ text, name, value, options, handleOnChange }) {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}</label>
-      <select id={name} name={name}>
-          <option disabled>Selecione uma opçao</option>            
-            {options.map((option) => (
-              <option value={option.id} key={option.id}>{option.name}</option>
-            ))}          
-        </select>
+      <select
+        id={name}
+        name={name}
+        onChange={handleOnChange}
+        value={value || ""}
+      >
+        <option disabled>Selecione uma opçao</option>
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
-export default Select
+export default Select;
--- costsVite/src/components/project/ProjectForm.jsx
+++ costsVite/src/components/project/ProjectForm.jsx
@@ -4,40 +4,70 @@ import Select from "../ci/Select";
 import SubmitButton from "../ci/SubmitButton";
 import styles from "../styles/ProjectForm.module.css";
 
-function ProjectForm({btnText}) {
-
-  const [categories, setCategories] = useState([])
+function ProjectForm({ handleSubmit, btnText, projectData }) {
+  const [category, setCategory] = useState([]);
+  const [project, setProject] = useState(projectData || {});
 
   useEffect(() => {
-    fetch("http://localhost:5000/categories", {
+    fetch("http://localhost:5000/category", {
       method: "GET",
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     })
-    .then((response) => response.json())
-    .then((data) => {
-      setCategories(data)
-    })
-    .catch((err) => console.log(err))
-  }, [])
+      .then((response) => response.json())
+      .then((data) => {
+        setCategory(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const submit = (e) => {
+    e.preventDefault();
+    //console.log(project);
+    handleSubmit(project)
+  };
+
+  function handleChange(e) {
+    setProject({ ...project, [e.target.name]: e.target.value });
+  }
+
+  function handleCategory(e) {
+    setProject({
+      ...project,
+      category: {
+        id: e.target.value,
+        name: e.target.options[e.target.selectedIndex].text,
+      },
+    });
+  }
 
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type="text"
         text="Nome do Projeto"
         name="name"
         placeholder="Insira o nome do projeto"
+        handleOnChange={handleChange}
+        value={project.name ? project.name : ''}
       />
       <Input
         type="number"
         text="Orçamento do Projeto"
         name="budget"
         placeholder="Insira o orçamento do projeto"
+        handleOnChange={handleChange}
+        value={project.budget ? project.budget : ''}
       />
-      <Select text="Selecione a categoria" name="category_id" options={categories}/>
-      <SubmitButton text={btnText}/>
+      <Select 
+        handleOnChange={handleCategory}
+        text="Selecione a categoria"
+        name="category_id"
+        options={category}
+        value={project.category ? project.category.id : ''}
+      />
+      <SubmitButton text={btnText} />
     </form>
   );
 }
